d0965a778adfdad520ee3276d09463082ff4d651
rael06 <rael06@hotmail.fr>
2019-06-19 11:19:20 +0200

    tuto done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField myField;
+
+    @FXML
+    private Button myButton;
+
+    @FXML
+    private void onClick() {
+        myButton.setDisable(true);
+        myField.setText("hello world");
+    }
 }
